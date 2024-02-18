8928c514f467e44229e2a5f348668dcc5ce89638
martyboggs <whack47@gmail.com>
2024-02-18 10:45:22 -0800

    ok art boy

--- go.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-magick img.png -crop 50%x+0+0 a1.png
-magick img.png -crop 50%x+928+0 a2.png
-magick img.png -crop 50%x+0+1232 a3.png
-magick img.png -crop 50%x+928+1232 a4.png
-
-# curl -d '{"style": "art", "noise": "3", "x2": "2", "input": "URL"}' -H 'X-API-KEY:4502c124653f4bed9ffade2c6bfc42af' https://bigjpg.com/api/task/
