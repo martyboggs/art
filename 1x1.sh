699145b0250b340291d9ec55f1870566b8b4fc66
martyboggs <whack47@gmail.com>
2024-02-18 17:30:04 -0800

    ok

---
+++ 1x1.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+i=0
+
+for file in 1x1/*; do 
+    if [[ $file == *.png ]]
+    then
+        magick $file -crop 50%x+0+0 $i.png
+        i=`expr $i + 1`
+        magick $file -crop 50%x+1024+0 $i.png
+        i=`expr $i + 1`
+        magick $file -crop 50%x+0+1024 $i.png
+        i=`expr $i + 1`
+        magick $file -crop 50%x+1024+1024 $i.png
+        i=`expr $i + 1`
+    fi
+done
+
+# curl -d '{"style": "art", "noise": "3", "x2": "2", "input": "URL"}' -H 'X-API-KEY:4502c124653f4bed9ffade2c6bfc42af' https://bigjpg.com/api/task/
